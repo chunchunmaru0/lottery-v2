d7d3b72d77d873b17918b3781c7f395547f790df
chunchunmaru0 <aalupidalu10@gmail.com>
2024-03-31 21:14:22 -0700

    feat: Send Emails for Lottery Winning

---
+++ components/Email/LotteryEmail/index.tsx
@@ -0,0 +1,66 @@
+import { LotteryType } from "@/components/Lottery/types";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import React from "react";
+
+interface Props {
+  type: LotteryType;
+  lottery: {
+    id: string;
+    userId: string;
+    lotteryType: string;
+    numbers: number[];
+    notified: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+}
+const LotteryEmail: React.FC<Props> = ({ type, lottery }) => {
+  const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL!;
+  const getPrize = async (lotteryNum: string) => {
+    const apiUrl = `${NEXT_PUBLIC_URL}/api/v2/check-lottery/${type.toLowerCase()}?userNumber=${lotteryNum}`;
+    const res = await fetch(apiUrl, { method: "POST" });
+    const data = await res.json();
+    console.log("prize", data?.prize);
+    return data?.prize?.win;
+  };
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Lottery</TableHead>
+
+          <TableHead className="text-right">Winning</TableHead>
+          <TableCaption>{type}</TableCaption>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {lottery.map((lot, i) => (
+          <TableRow key={i}>
+            <TableCell className="w-[450px] font-medium">
+              {/* <LotteryBalls
+              type="Megamillions"
+              latestMmWinningNumber={latestMmWinningNumber}
+            /> */}
+              {lot.numbers.join(",")}
+            </TableCell>
+
+            <TableCell className="ml-16">
+              $ {getPrize(lot.numbers.join(","))}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default LotteryEmail;
